da641e5c18ff4059a71b8b1a7bff0237d8f767d9
komalmauryagithub <146904888+komalmauryagithub@users.noreply.github.com>
2023-10-11 09:36:58 +0530

    Add files via upload

---
+++ fycs 43.py
@@ -0,0 +1,9 @@
+num =(1,2,3,4,5,6,7,8,9,10,11)
+#count is veriable ,and i have initialised it by 0 value.
+count=0
+while (count <12):
+    print(num[count])
+    count=count+1
+    if count ==11:
+        break
+print("End of Execution ")
